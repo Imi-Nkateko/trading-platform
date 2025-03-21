e28e08df9c5e9cb42beb7f88e6710dd21738114e
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-03-22 00:20:40 +0200

    Update StockOverview.jsx
    import page components to display stock list and search input

--- client/src/pages/StockOverview.jsx
+++ client/src/pages/StockOverview.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import AutoComplete from "../components/AutoComplete";
+import StockList from "../components/StockList";
 
 const StockOverview = () => {
-  return <div>StockOverview</div>;
+  return (
+    <div>
+      StockOverview
+      <AutoComplete />
+      <StockList />
+    </div>
+  );
 };
 
 export default StockOverview;

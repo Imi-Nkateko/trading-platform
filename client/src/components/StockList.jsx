6f8907c274c7c43c9dd0ab1bd9a7d8ffca3fa5e4
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-03-22 00:19:51 +0200

    Create StockList.jsx

---
+++ client/src/components/StockList.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+
+const StockList = () => {
+    const [watchlist, setWatchlist] = useState(["GOOGL", "MSFT", "AMZN", "TLSA"])
+  return <div>StockList</div>;
+};
+
+export default StockList;

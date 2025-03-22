30f4d9bca00dd8742bf4a2cb66274d8c72a2688a
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-03-22 23:45:51 +0200

    Update StockList.jsx
    fetch api and display stocks

--- client/src/components/StockList.jsx
+++ client/src/components/StockList.jsx
@@ -1,8 +1,74 @@
 import { useState, useEffect } from "react";
+import finnhub from "../apis/finnhub";
 
 const StockList = () => {
-    const [watchlist, setWatchlist] = useState(["GOOGL", "MSFT", "AMZN", "TLSA"])
-  return <div>StockList</div>;
+  const [stock, setStock] = useState([]);
+  const [watchlist, setWatchlist] = useState(["GOOGL", "MSFT", "AMZN", "TLSA"]);
+
+  useEffect(() => {
+    let isMounted = true;
+    const fetchData = async () => {
+      try {
+        const responses = await Promise.all(
+          watchlist.map((stock) => {
+            return finnhub.get("/quote", {
+              params: {
+                Symbol: stock,
+              },
+            });
+          })
+        );
+        console.log(responses);
+        const data = responses.map((response) => {
+          return {
+            data: response.data,
+            symbol: response.config.params.symbol,
+          };
+        });
+        console.log(data);
+        if (isMounted) {
+          setStock(data);
+        }
+      } catch (error) {}
+    };
+    fetchData();
+    return () => (isMounted = false);
+  }, []);
+
+  return (
+    <div>
+      <table className="table hover mt-5">
+        <thead style={{ color: "rgb(79,89,102)" }}>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Last</th>
+            <th scope="col">Chg</th>
+            <th scope="col">Chg%</th>
+            <th scope="col">High</th>
+            <th scope="col">Low</th>
+            <th scope="col">Open</th>
+            <th scope="col">PClose</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stock.map((stockData) => {
+            return (
+              <tr className="table-row " >
+                <th scope="row">{stockData.symbol}</th>
+                <td>{stockData.data.c}</td>
+                <td>{stockData.data.d}</td>
+                <td>{stockData.data.dp}</td>
+                <td>{stockData.data.h}</td>
+                <td>{stockData.data.l}</td>
+                <td>{stockData.data.o}</td>
+                <td>{stockData.data.pc}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default StockList;
